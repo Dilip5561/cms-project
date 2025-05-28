b748b6f0a8592484e19c0db6b0174ca80cc480df
Dilip5561 <dilipfs001@gmail.com>
2025-05-28 07:22:12 +0000

    check

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+   allowedHosts:["5173-dilip5561-cms-project-031n4t13y0.app.codeanywhere.com"]
+  },
   plugins: [react()],
 })
